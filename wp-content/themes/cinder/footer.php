3c88d509f635b2eb27c1fb5c66baf8bb1849d453
Melusi Khumalo <melusi-k@users.noreply.github.com>
2021-03-22 07:59:17 +0200

    Update config
    ?

--- wp-content/themes/cinder/footer.php
+++
@@ -1,5 +0,0 @@
-<?php get_template_part( 'template-parts/footer-big' ); ?>
-<?php get_template_part( 'template-parts/modal-search' ); ?>
-<?php wp_footer(); ?>
-</body>
-</html>
--- wp-content/themes/cinder/sidebar-right.php
+++
@@ -1,7 +0,0 @@
-						<!-- sidebar right start -->
-						<aside class="">
-							<div class="sidebar sidebar-right">
-							<?php dynamic_sidebar( 'sidebar-right' ); ?>
-							</div>
-						</aside>
-						<!-- sidebar end -->
